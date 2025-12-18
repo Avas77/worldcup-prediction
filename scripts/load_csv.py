ccdea707d65419ef587249f2e5f1da51efe865e1
Avas Bajracharya <insanebuzz09@gmail.com>
2025-12-18 10:32:18 -0500

    loaded csv file from kaggle

---
+++ scripts/load_csv.py
@@ -0,0 +1,34 @@
+import pandas as pd
+
+df = pd.read_csv('./data/raw/results.csv')
+
+df['date'] = pd.to_datetime(df['date'])
+df = df[df['date'] >= '2010-01-01']
+
+df['home_win'] = (df['home_score'] > df['away_score']).astype(int)
+
+df = df.dropna(subset=["home_score", "away_score", "home_team", "away_team"])
+
+home = df[['date', 'home_team', 'away_team', 'home_score', 'away_score', 'home_win', "neutral"]].copy()
+home.columns = ['date', 'team', 'opponent', 'team_score', 'opponent_score', 'win', "neutral"]
+
+away = df[[
+    "date", "away_team", "home_team",
+    "away_score", "home_score", "home_win", "neutral"
+]].copy()
+away.columns = ["date", "team", "opponent", "team_score", "opponent_score", "win", "neutral"]
+away["win"] = (away["team_score"] > away["opponent_score"]).astype(int)
+
+matches_long = pd.concat([home, away], ignore_index=True)
+matches_long = matches_long.sort_values(by=['team', 'date'])
+matches_long["avg_goals_last_5"] = (
+    matches_long.groupby("team")["team_score"]
+    .transform(lambda x: x.rolling(window=5, min_periods=1).mean().shift(1))
+)
+matches_long["win_rate_last_5"] = (
+    matches_long.groupby("team")["win"]
+    .transform(lambda x: x.rolling(window=5, min_periods=1).mean().shift(1))
+)
+matches_long = matches_long.sort_values(by=['date', 'team'])
+matches_long.dropna(inplace=True)
+matches_long.to_csv('./data/features/team_features.csv', index=False)
